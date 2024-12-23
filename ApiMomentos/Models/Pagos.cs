220c3d46b60dad3703e10c205d3fd7c1cdfc790f
Federico <fmartin@excelenciadigital.net>
2024-12-23 16:22:15 -0300

    Arreglo en modelo pagos

--- ApiMomentos/Models/Pagos.cs
+++ ApiMomentos/Models/Pagos.cs
@@ -6,7 +6,7 @@ namespace ApiObjetos.Models;
 
 public partial class Pagos
 {
-    public int? PagoId { get; set; }
+    public int PagoId { get; set; }
 
     public decimal? MontoEfectivo { get; set; }
 
@@ -20,12 +20,8 @@ public partial class Pagos
 
     public int? CierreId { get; set; }
     public DateTime? fechaHora { get; set; }    
-<<<<<<< HEAD
     public string? Observacion { get; set; }
-=======
-    public string Observacion { get; set; }
-    public Recargos Recargo { get; set; }
->>>>>>> 5176191ba218d0c4695e0c984b64ffbe642bdc32
+    public Recargos? Recargo { get; set; }
     public virtual Cierre? Cierre { get; set; }
     [JsonIgnore]
     public virtual MediosPago? MedioPago { get; set; }
