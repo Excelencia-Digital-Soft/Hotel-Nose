d322e12b7f866a646e267e6100539c3fe59fae0d
Federico <fmartin@excelenciadigital.net>
2025-03-17 09:17:31 -0300

    arreglo

--- src/store/websocket.js
+++ src/store/websocket.js
@@ -3,7 +3,6 @@ import { useAuthStore } from "./auth.js";
 import * as signalR from "@microsoft/signalr";
 
 export const useWebSocketStore = defineStore("websocket", {
-<<<<<<< HEAD
     state: () => ({
         connection: null,
         notifications: [],
@@ -48,52 +47,6 @@ export const useWebSocketStore = defineStore("websocket", {
                 console.error("WebSocket connection error:", error);
             }
         },
-=======
-       state: () => ({
-           connection: null,
-           notifications: [],
-           nextNotificationId: 1,
-           eventCallbacks: {}, 
-       }),
-       actions: {
-           async connect() {
-               const authStore = useAuthStore();
-               const institucionID = authStore.institucionID;
-   
-               if (!institucionID) {
-                   console.error("No InstitucionID found, cannot connect to WebSocket.");
-                   return;
-               }
-   
-               console.log("Connecting to WebSocket with InstitucionID:", institucionID);
-   
-               this.connection = new signalR.HubConnectionBuilder()
-                   .withUrl(`${import.meta.env.VITE_API_BASE_URL}notifications`)
-                   .withAutomaticReconnect()
-                   .build();
-   
-               try {
-                   await this.connection.start();
-                   console.log("WebSocket connected, subscribing to institution:", institucionID);
-   
-                   await this.connection.invoke("SubscribeToInstitution", institucionID.toString());
-   
-                   this.connection.on("ReceiveNotification", (notification) => {
-                       console.log("New notification:", notification);
-                       
-                       // Notify all registered components
-                       Object.values(this.eventCallbacks).forEach(callback => {
-                           if (typeof callback === "function") {
-                               callback(notification);
-                           }
-                       });
-                       this.addNotification(notification);
-                   });
-               } catch (error) {
-                   console.error("WebSocket connection error:", error);
-               }
-           },
->>>>>>> 860413e0a95ce2fd21bb4037a3dcd9dd30139933
 
         addNotification(notification) {
             const id = this.nextNotificationId++;
